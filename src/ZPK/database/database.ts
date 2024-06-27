25f2fca8ef6801aecaa0e8eceaec1452489b2f15
wuw-shz <wuw62481@gmail.com>
2024-06-27 19:23:17 +0700

    @wuw.sh

--- src/ZPK/database/database.ts
+++ src/ZPK/database/database.ts
@@ -4,11 +4,19 @@ import { InitialData, InitialDataType, Key, Value } from "@zpk";
 export function Database(player: Player): InitialDataType {
     return new Proxy(InitialData, {
         get(_, key: Key) {
-            return player.getDynamicProperty(key) ?? InitialData[key];
+            try {
+                return JSON.parse((player.getDynamicProperty(key) ?? InitialData[key]) as string);
+            } catch {
+                return player.getDynamicProperty(key) ?? InitialData[key];
+            }
         },
-        set(_, key: Key, value: Value) {
-            player.setDynamicProperty(key, typeof value == "number" ? (isFinite(value) ? value : "N/A") : value);
-            return true;
+        set(_, key: Key, value) {
+            try {
+                player.setDynamicProperty(key, typeof value == "number" ? (isFinite(value) ? value : "N/A") : value);
+            } catch {
+                player.setDynamicProperty(key, JSON.stringify(value));
+            }
+            return true
         },
     });
 }
