334d87ec1203be2361dc4535802fb1e2b5b0b1d7
wuw-shz <wuw62481@gmail.com>
2024-06-28 15:58:08 +0700

    'auto-save'

--- src/ZPK/main.ts
+++ src/ZPK/main.ts
@@ -13,7 +13,7 @@ world.beforeEvents.itemUse.subscribe((data) => {
     if (item.typeId === "minecraft:compass" && player.isSneaking) {
         if (!db.toggleZPKMod) db.toggleZPKMod = true;
         else db.toggleZPKMod = false;
-    } else if (item.typeId === "minecraft:comwewwwwswpaass") {
+    } else if (item.typeId === "minecraft:comwewwwwswpaassw") {
         settingUI(player);
     }
 });
