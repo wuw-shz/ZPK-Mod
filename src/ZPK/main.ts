36107cc3010139789784417b9d5d69937d729c86
wuw-shz <wuw62481@gmail.com>
2024-06-29 12:58:27 +0700

    Auto Save

--- src/ZPK/main.ts
+++ src/ZPK/main.ts
@@ -39,7 +39,7 @@ system.runInterval(() => {
 
         updatePlayerState(player, db);
         handleLandingState(player, db);
-        handleMovementState(player, db,);
+        handleMovementState(player, db, );
         updateGuiDisplay(player, db);
     }
 });
