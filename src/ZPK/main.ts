a6b070d962060bd6ebe5b52378297e33dd5c6b32
wuw-shz <wuw62481@gmail.com>
2024-06-29 13:06:19 +0700

    Auto Save

--- src/ZPK/main.ts
+++ src/ZPK/main.ts
@@ -34,7 +34,6 @@ system.runInterval(() => {
             const vel = player.getVelocity();
             const fullVel = Math.sqrt(vel.x ** 2 + vel.z ** 2);
             updateGuiDisplay(player, db);
-
             updatePlayerState(player, db, fullVel);
             handleLandingState(player, db);
             handleMovementState(player, db, fullVel);
