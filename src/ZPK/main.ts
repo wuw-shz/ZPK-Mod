2dfc5df17336bd67e572d1190bf7711e1cf05e11
wuw-shz <wuw62481@gmail.com>
2024-06-29 13:06:18 +0700

    Auto Save

--- src/ZPK/main.ts
+++ src/ZPK/main.ts
@@ -31,13 +31,13 @@ system.runInterval(() => {
         const db = Database(player);
 
         if (!db.toggleZPKMod) {
-        const vel = player.getVelocity();
-        const fullVel = Math.sqrt(vel.x ** 2 + vel.z ** 2);
+            const vel = player.getVelocity();
+            const fullVel = Math.sqrt(vel.x ** 2 + vel.z ** 2);
             updateGuiDisplay(player, db);
 
-        updatePlayerState(player, db, fullVel);
-        handleLandingState(player, db);
-        handleMovementState(player, db, fullVel);
+            updatePlayerState(player, db, fullVel);
+            handleLandingState(player, db);
+            handleMovementState(player, db, fullVel);
             continue;
         }
     }
