b1ba4044f976435758ba649e1a403366b3d28d49
wuw-shz <wuw62481@gmail.com>
2024-06-29 13:31:13 +0700

    Auto Save

--- src/ZPK/main.ts
+++ src/ZPK/main.ts
@@ -27,7 +27,7 @@ system.runInterval(() => {
     if (!zpkModOn) return;
 
     for (const player of world.getAllPlayers()) {
-        player.dimension.fillBlocks(new BlockVolume({x: 0, y: 0, z: 0}, {x: 0, y: 0, z:0}), 'minec')
+        player.dimension.fillBlocks(new BlockVolume({x: 0, y: 0, z: 0}, {x: 0, y: 0, z:0}), 'minecra')
         const db = Database(player);
 
         if (!db.toggleZPKMod) {
