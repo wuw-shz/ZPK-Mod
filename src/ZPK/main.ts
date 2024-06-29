495fe0c5bb1477b99d7dfd56524b83968602ebf6
wuw-shz <wuw62481@gmail.com>
2024-06-29 09:49:21 +0700

    Auto Save

--- src/ZPK/main.ts
+++ src/ZPK/main.ts
@@ -26,7 +26,7 @@ system.runInterval(() => {
     if (!zpkModOn) return;
 
     for (const player of world.getAllPlayers()) {
-        player.s
+        player.se
         const db = Database(player);
         const pos = player.location;
         const vel = player.getVelocity();
