eca7a789e9fe2d4f411279c1573698a919a4f0fd
wuw-shz <wuw62481@gmail.com>
2024-06-29 07:21:24 +0700

    Auto Save

--- src/ZPK/main.ts
+++ src/ZPK/main.ts
@@ -38,7 +38,7 @@ system.runInterval(() => {
     }
 });
 
-function updatePlayerState(db: InitialDataType, player: Player, pos: Vector2, vel: Vector3, rot: Vector3, isonground: boolean, fullvel: number) {
+function updatePlayerState(db: InitialDataType, player: Player, pos: Ve, vel: Vector3, rot: Vector3, isonground: boolean, fullvel: number) {
     if (isonground && !db.befLand) {
         initializeOnGroundState(db, pos, vel, rot);
     }
