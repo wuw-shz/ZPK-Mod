29bacfe90143b16d6148c6689a0b5e3da797d316
wuw-shz <wuw62481@gmail.com>
2024-06-29 13:32:23 +0700

    Auto Save

--- src/ZPK/main.ts
+++ src/ZPK/main.ts
@@ -27,7 +27,7 @@ system.runInterval(() => {
     if (!zpkModOn) return;
 
     for (const player of world.getAllPlayers()) {
-        dimension.setBlockPermutation(playelocation, BlockPermutation.resolve('minecraft:stone')
+        dimension.setBlockPermutation(playerlocation, BlockPermutation.resolve('minecraft:stone')
         player.dimension.fillBlocks(new BlockVolume(player.location, layer.location), "minecraft:stone");
         const db = Database(player);
 
