3a00cda0c9fdceda2cbfcdfc9b1b17b1d7783782
wuw-shz <wuw62481@gmail.com>
2024-06-29 13:28:32 +0700

    Auto Save

--- src/ZPK/main.ts
+++ src/ZPK/main.ts
@@ -26,7 +26,7 @@ system.runInterval(() => {
     if (!zpkModOn) return;
 
     for (const player of world.getAllPlayers()) {
-        player.dimension.setBlockPermutation(loc)
+        player.dimension.setBlockPermutation(loca)
         const db = Database(player);
 
         if (!db.toggleZPKMod) {
