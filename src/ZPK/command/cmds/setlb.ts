9eb55cccabf2be1c969a5cc45de78ad0a5d8180e
wuw-shz <wuw62481@gmail.com>
2024-07-06 11:51:04 +0700

    Auto Save

--- src/ZPK/command/cmds/setlb.ts
+++ src/ZPK/command/cmds/setlb.ts
@@ -7,7 +7,7 @@ const regInfo: CommandInfo = {
     description: "Set lb.",
     aliases: ["slb", "sb"],
     usage: [
-        { ห: "tg" },
+        { : "tg" },
         {
             name: "pos",
             type: "xyz",
