116857b1e87f4f067714a5e3a1b54337046ca172
wuw-shz <wuw62481@gmail.com>
2024-07-06 12:06:19 +0700

    Auto Save

--- src/ZPK/command/cmds/clearpb.ts
+++ src/ZPK/command/cmds/clearpb.ts
@@ -17,6 +17,6 @@ registerCommand(regInfo, (player, msg, args) => {
     db.pb = Infinity;
     db.pbx = Infinity;
     db.pbz = Infinity;
-    if (a)
+    if (args)
     print("§l§a✔ Clear pb and offset!", player);
 });
