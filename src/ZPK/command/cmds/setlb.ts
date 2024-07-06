4c04f46afcd1922694aa8fd1a85a71746452b596
wuw-shz <wuw62481@gmail.com>
2024-07-06 10:42:08 +0700

    Auto Save

--- src/ZPK/command/cmds/setlb.ts
+++ src/ZPK/command/cmds/setlb.ts
@@ -10,7 +10,7 @@ const regInfo: CommandInfo = {
         {
             name: "pos",
             type: "xyz",
-            
+            de
         },
         { name: "target", type: "string", default: "" },
     ],
