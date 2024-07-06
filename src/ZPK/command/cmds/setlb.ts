b7b0d7ae7b8f0bf6e2d5e89a79badefb582f6f22
wuw-shz <wuw62481@gmail.com>
2024-07-06 12:20:51 +0700

    Auto Save

--- src/ZPK/command/cmds/setlb.ts
+++ src/ZPK/command/cmds/setlb.ts
@@ -36,7 +36,7 @@ registerCommand(regInfo, (player, msg, args) => {
         Server.command.callCommand(player, "clearpb", ["-s"]);
         print(`§l§a✔ Set lb! at pos (${db.lb.x}, ${db.lb.y}, ${db.lb.z})`, player);
     } else {
-        print(`§l§c✘ Invalid input! Using\n"${cmdPrefix}setlb <tartarget, tg>"\n"${cmdPrefix}setlb <position: x y z>"`, player);
+        print(`§l§c✘ Invalid input! Using\n"${cmdPrefix}setlb <targetarget, tg>"\n"${cmdPrefix}setlb <position: x y z>"`, player);
     }
 });
 
