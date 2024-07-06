276be2fcdf6936b083bc163e9b8f287245e9a607
wuw-shz <wuw62481@gmail.com>
2024-07-06 12:02:10 +0700

    Auto Save

--- src/ZPK/command/cmds/setlb.ts
+++ src/ZPK/command/cmds/setlb.ts
@@ -21,14 +21,14 @@ registerCommand(regInfo, (player, msg, args) => {
         const pos = CommandPosition.parseArgs(positionFormat(args.get("target") as string), 0).result.relativeTo(player, true);
         db.lbon = true;
         db.lb = vec3(Math.floor(pos.x), pos.y, Math.floor(pos.z));
+        Server.command.callCommand(player, "clearpb");
+        print(`§l§a✔ Set lb here! at (${db.lb.x}, ${db.lb.y}, ${db.lb.z})`, player);
+    } else if (["target", "tg"].includes(args.get("target") as string)) {
         const target = player.getBlockFromViewDirection().block;
         db.lbon = true;
         db.lb = vec3(Math.floor(target.x), target.y + 1, Math.floor(target.z));
         Server.command.callCommand(player, "clearpb");
         print(`§l§a✔ Set lb target block! at (${db.lb.x}, ${db.lb.y}, ${db.lb.z})`, player);
-        Server.command.callCommand(player, "clearpb");
-        print(`§l§a✔ Set lb here! at (${db.lb.x}, ${db.lb.y}, ${db.lb.z})`, player);
-    } else if (["target", "tg"].includes(args.get("target") as string)) {
     } else {
         print(`§l§c✘ Invalid target input! Using "${cmdPrefix}setlb [target, tg]"`, player);
     }
