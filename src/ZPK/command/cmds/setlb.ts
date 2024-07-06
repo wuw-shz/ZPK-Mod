92b39d908fad92ac4f808bfb240083a66655c256
wuw-shz <wuw62481@gmail.com>
2024-07-06 12:16:38 +0700

    Auto Save

--- src/ZPK/command/cmds/setlb.ts
+++ src/ZPK/command/cmds/setlb.ts
@@ -27,7 +27,7 @@ registerCommand(regInfo, (player, msg, args) => {
         db.lb = vec3(Math.floor(target.x), target.y + 1, Math.floor(target.z));
         Server.command.callCommand(player, "clearpb", ["-s"]);
         print(`§l§a✔ Set lb target block! at (${db.lb.x}, ${db.lb.y}, ${db.lb.z})`, player);
-    } else if (CommandPosition.parseArgs(positionFormat(args.get("target") + args.get("pos2") + args.get('')), 0).result.relativeTo(player, true) !== null) {
+    } else if (CommandPosition.parseArgs(positionFormat(args.get("target") + args.get("pos2") + args.get('p')), 0).result.relativeTo(player, true) !== null) {
         const pos = CommandPosition.parseArgs(positionFormat(args.get("target") as string), 0).result.relativeTo(player, true);
         db.lbon = true;
         db.lb = vec3(Math.floor(pos.x), pos.y, Math.floor(pos.z));
