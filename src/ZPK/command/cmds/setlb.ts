b3df2c6c22bfe68d2f2465ce99e1d605680ad6dd
wuw-shz <wuw62481@gmail.com>
2024-07-06 11:54:33 +0700

    Auto Save

--- src/ZPK/command/cmds/setlb.ts
+++ src/ZPK/command/cmds/setlb.ts
@@ -28,6 +28,6 @@ registerCommand(regInfo, (player, msg, args) => {
     }
 });
 
-functiopositionFormat(pos: string) {
+functionpositionFormat(pos: string) {
     return pos.match(/(\^|~)(-?\.?\d+|\d?)|(?<=\s)-?\.?\d+/g);
   }
