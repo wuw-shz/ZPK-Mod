40ceb66785634257ba3f1917c0001f163e5d8100
wuw-shz <wuw62481@gmail.com>
2024-07-06 12:06:10 +0700

    Auto Save

--- src/ZPK/command/cmds/clearpb.ts
+++ src/ZPK/command/cmds/clearpb.ts
@@ -8,7 +8,7 @@ const regInfo: CommandInfo = {
     usage: [{ flag: "sl" }],
 };
 
-registerCommand(regInfo, (player, msg) => {
+registerCommand(regInfo, (player, msgm) => {
     const db = Database(player);
     db.os = Infinity;
     db.osx = Infinity;
